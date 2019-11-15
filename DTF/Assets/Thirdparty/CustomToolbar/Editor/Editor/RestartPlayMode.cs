72537681ac29289f15d63f18741011c0d980e900
Team-on <timonsol13@gmail.com>
2019-11-15 13:37:07 +0200

    Add custom toolbar

---
+++ DTF/Assets/Thirdparty/CustomToolbar/Editor/Editor/RestartPlayMode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityToolbarExtender {
+
+	[InitializeOnLoad]
+	public static class RestartPlayMode {
+
+		static RestartPlayMode() {
+			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+		}
+		static void OnToolbarGUI() {
+
+
+			EditorGUIUtility.SetIconSize(new Vector2(17, 17));
+			if (GUILayout.Button(EditorGUIUtility.IconContent("LookDevResetEnv@2x"), ToolbarStyles.commandButtonStyle)) {
+				if (EditorApplication.isPlaying) {
+					SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+				}
+			}
+
+
+			Time.timeScale = EditorGUILayout.Slider("", Time.timeScale, 0.1f, 2f, GUILayout.Width(150));
+		}
+	}
+}
