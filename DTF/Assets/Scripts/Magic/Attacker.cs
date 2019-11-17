4e7afe4ee65446fa441f1619b94bc18855242ebf
Team-on <timonsol13@gmail.com>
2019-11-17 04:07:31 +0200

    Firaball stats based on mouse hold time

---
+++ DTF/Assets/Scripts/Magic/Attacker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class Attacker : NetworkBehaviour {
+	[SyncVar] public bool isCanDamage = true;
+	[SyncVar] [NonSerialized] public int playerIndex;
+	public int damage = 10;
+
+	[SerializeField] protected VisualEffect visualEffect;
+
+	public virtual void OnReleaseMouse(Vector3 pos) { }
+
+	[Server]
+	public void OnCollideWithHealth() {
+		isCanDamage = false;
+		RpcOnAttack();
+	}
+
+	[ClientRpc]
+	protected void RpcOnAttack() {
+		StopAllCoroutines();
+		visualEffect.Stop();
+		foreach (var collider in GetComponents<Collider>())
+			collider.enabled = false;
+		LeanTween.delayedCall(gameObject, 3.0f, () => NetworkServer.Destroy(gameObject));
+	}
+
+	public override void OnNetworkDestroy() {
+		base.OnNetworkDestroy();
+		LeanTween.cancel(gameObject, false);
+		StopAllCoroutines();
+	}
+}
---
+++ DTF/Assets/Scripts/Magic/Fireball.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class Fireball : Attacker {
+	[SyncVar] float currHoldTime = 0;
+
+	[SerializeField] float holdTimeMax = 10;
+	[SerializeField] float sizeMin = 0.1f;
+	[SerializeField] float sizeMax = 2;
+	[SerializeField] float spawnRateMin = 150;
+	[SerializeField] float spawnRateMax = 3000;
+	[SerializeField] float speedMin = 20;
+	[SerializeField] float speedMax = 5;
+	[SerializeField] float moveDistanceMin = 20;
+	[SerializeField] float moveDistanceMax = 5;
+	[SerializeField] float damageMin = 5;
+	[SerializeField] float damageMax = 50;
+
+	[SerializeField] float moveToPosAccurasity = 0.25f;
+	Vector3 moveToPos;
+
+	[SerializeField] SphereCollider collider;
+
+	void Awake() {
+		moveToPosAccurasity *= moveToPosAccurasity;
+	}
+
+	void Update() {
+		if (!isServer)
+			return;
+
+		if(currHoldTime < holdTimeMax) {
+			currHoldTime += Time.deltaTime;
+			if (currHoldTime > holdTimeMax)
+				currHoldTime = holdTimeMax;
+			RpcOnUpdate();
+		}
+	}
+
+	[ClientRpc]
+	protected void RpcOnUpdate() {
+		visualEffect.SetInt("SpawnRate", Mathf.RoundToInt(Mathf.Lerp(spawnRateMin, spawnRateMax, currHoldTime / holdTimeMax)));
+		float radius = Mathf.Lerp(sizeMin, sizeMax, currHoldTime / holdTimeMax);
+		visualEffect.SetFloat("SphereRadius", radius);
+		collider.radius = radius;
+		damage = Mathf.RoundToInt(Mathf.Lerp(damageMin, damageMax, currHoldTime / holdTimeMax));
+	}
+
+	public override void OnReleaseMouse(Vector3 pos) {
+		moveToPos = (pos - transform.position).normalized * Mathf.Lerp(moveDistanceMin, moveDistanceMax, currHoldTime / holdTimeMax) + transform.position;
+		StartCoroutine(MoveCoroutine());
+	}
+
+	IEnumerator MoveCoroutine() {
+		transform.rotation = Quaternion.LookRotation(moveToPos - transform.position);
+		float currSpeed = Mathf.Lerp(speedMin, speedMax, currHoldTime / holdTimeMax);
+
+		while ((transform.position - moveToPos).sqrMagnitude > moveToPosAccurasity) {
+			transform.Translate(Vector3.forward * currSpeed * Time.deltaTime, Space.Self);
+			yield return null;
+		}
+
+		RpcOnAttack();
+	}
+}
